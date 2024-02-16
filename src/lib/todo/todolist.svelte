b88b97f19bbe0f6489b6bc672829d59450a88492
Nicholas Wold <nick@lunadb.io>
2024-02-16 15:35:46 -0800

    got checkboxes working

---
+++ src/lib/todo/todolist.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+</script>
+
+<div class="border rounded-md p-6">
+	<h1 class="font-bold mb-4">Task list</h1>
+
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Task from '$lib/todo/task.svelte';
+	import Todolist from '$lib/todo/todolist.svelte';
+</script>
+
+<Todolist>
+	<Task title="Test task"></Task>
+</Todolist>
